79a66408b98a6d7c474d84f9364d857602208ff8
Michael Strauß <strauss.michael@gmx.net>
2022-08-24 14:51:44 +0200

    different player images for 6 players added

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-player',
@@ -7,8 +8,20 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
   @Input() name: any;
+  @Input() nbrOfPlayers: number | any;
   @Input() playerActive: boolean = false;
-  constructor() { }
+
+  
+  imagePlayer: string = '';
+
+  userImages = ['user-img-1.jpg', 'user-img-4.svg', 'user-img-3.svg', 'user-img-5.svg', 'user-img-2.svg', 'user-img-6.svg']
+ 
+
+  constructor() {
+    console.log(this.nbrOfPlayers)
+    console.log(this.name)
+    
+   }
 
   ngOnInit(): void {
   }
