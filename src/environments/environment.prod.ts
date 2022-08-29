d6fced1b557e0862ca08863c63d7dd59ae6a0e42
Michael Strauß <strauss.michael@gmx.net>
2022-08-29 13:29:59 +0200

    firebase reset

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyAdxFYPLGcoWa3iXPEAT5VEjszhReiQ-vM",
-    authDomain: "ring-of-fire-f1cee.firebaseapp.com",
-    projectId: "ring-of-fire-f1cee",
-    storageBucket: "ring-of-fire-f1cee.appspot.com",
-    messagingSenderId: "94377126297",
-    appId: "1:94377126297:web:586060ce3494f995c40b08"
+    projectId: 'ring-of-fire-f1cee',
+    appId: '1:94377126297:web:586060ce3494f995c40b08',
+    databaseURL: 'https://ring-of-fire-f1cee-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'ring-of-fire-f1cee.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAdxFYPLGcoWa3iXPEAT5VEjszhReiQ-vM',
+    authDomain: 'ring-of-fire-f1cee.firebaseapp.com',
+    messagingSenderId: '94377126297',
   },
   production: true
 };
