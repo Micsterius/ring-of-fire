69b0e3743ed135a80ab8811c2458afcde8c47057
Michael Strauß <strauss.michael@gmx.net>
2022-08-29 06:02:29 +0200

    firebase deploy bugs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'ring-of-fire-f1cee',
-    appId: '1:94377126297:web:1d8a9a08dbdf2d9dc40b08',
-    storageBucket: 'ring-of-fire-f1cee.appspot.com',
-    apiKey: 'AIzaSyAdxFYPLGcoWa3iXPEAT5VEjszhReiQ-vM',
-    authDomain: 'ring-of-fire-f1cee.firebaseapp.com',
-    messagingSenderId: '94377126297',
+    apiKey: "AIzaSyAdxFYPLGcoWa3iXPEAT5VEjszhReiQ-vM",
+    authDomain: "ring-of-fire-f1cee.firebaseapp.com",
+    projectId: "ring-of-fire-f1cee",
+    storageBucket: "ring-of-fire-f1cee.appspot.com",
+    messagingSenderId: "94377126297",
+    appId: "1:94377126297:web:586060ce3494f995c40b08"
   },
   production: true
 };
